42567998e09862f5f1f8c0d9682a99638fbb9dfd
mani.f <78233841+manifarizi@users.noreply.github.com>
2022-09-12 20:32:59 +0430

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -24,6 +24,16 @@ DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULL
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
 
+def make_house():
+    old_ie = 0
+    for x, (ie, i) in zip('12345123451234512345123451234512345', zip('00000111112222233333444445555566666', [0, 1, 2, 3, 4, 40, 41, 42, 43, 44, 80, 81, 82, 83, 84, 120, 121, 122, 123, 124, 160, 161, 162, 163, 164])):
+        i = i + 6
+        y = int(ie)
+        if int(ie) == 6:
+            DISPLAYSURF.blit(pygame.image.load(dirname(__file__) + '\\texures\\overworld\\tile000', (((int(x) + 6) + 10) * 16, (8 + y) * 16)))
+        DISPLAYSURF.blit(pygame.image.load(dirname(__file__) + '\\texures\\overworld\\tile' + '0' * (3 - len(str(i))) + str(i) + '.png'), (((int(x) + 6) + 10) * 16, (8 + y) * 16))
+        old_ie = ie
+
 g1_list = []
 g2_list = []
 g3_list = []
@@ -65,6 +75,7 @@ tree_br = pygame.image.load(dirname(__file__) + '/texures/overworld/tile686.png'
 gui_heart = [pygame.transform.scale(pygame.image.load(dirname(__file__) + '/texures/Heart_5.png'), (32, 32)), pygame.transform.scale(pygame.image.load(dirname(__file__) + '/texures/Heart_4.png'), (32, 32)), pygame.transform.scale(pygame.image.load(dirname(__file__) + '/texures/Heart_3.png'), (32, 32)), pygame.transform.scale(pygame.image.load(dirname(__file__) + '/texures/Heart_2.png'), (32, 32)), pygame.transform.scale(pygame.image.load(dirname(__file__) + '/texures/Heart_1.png'), (32, 32))]
 
 gui_heart.reverse()
+
 def reload():
     for y in range(0, SCREEN_HEIGHT, 16):
         for x in range(0, SCREEN_WIDTH, 16):
@@ -84,19 +95,22 @@ def reload():
         DISPLAYSURF.blit(tree_tr, (i[0], i[1] + 16))
         DISPLAYSURF.blit(tree_bl, (i[0] + 16, i[1]))
         DISPLAYSURF.blit(tree_br, (i[0] + 16, i[1] + 16))
-
+    make_house()
     DISPLAYSURF.blit(gui_heart[P1.health], (16, 16))
     pygame.display.update()
 
 class Player(pygame.sprite.Sprite):
+    global FPS
     def __init__(self):
         super().__init__()
-        self.health = 4
+        self.isJump = False
+        self.health = 0
         self.image = pygame.image.load(dirname(__file__) + "/texures/player_stand_forwards.png")
         self.rect = self.image.get_rect()
         self.rect.center = (160, 520)
 
     def update(self):
+        global FPS
         pressed_keys = pygame.key.get_pressed()
         old_location = (self.rect.x, self.rect.y)
         if self.rect.top > 0:
@@ -122,6 +136,21 @@ class Player(pygame.sprite.Sprite):
                 self.rect.move_ip(5, 0)
                 for i in range(1, 3):
                     self.image = pygame.image.load(dirname(__file__) + "/texures/player_walk_right_" + str(i) + '.png')
+
+        if pressed_keys[K_SPACE]:
+            for i in range(40):
+                self.rect.move_ip(0, -1)
+                P1.draw(DISPLAYSURF)
+                make_house()
+                pygame.display.update()
+            for i in range(20):
+                pygame.time.delay(5)
+                self.rect.move_ip(0, 2)
+                P1.draw(DISPLAYSURF)
+                make_house()
+                pygame.display.update()
+            
+
         pygame.draw.line(DISPLAYSURF, (255, 255, 255), old_location, (self.rect.x, self.rect.y), 2)
         DISPLAYSURF.blit(gui_heart[P1.health], (16, 16))
 
@@ -133,12 +162,12 @@ P1 = Player()
 while True:
     reload()
     for event in pygame.event.get():
-                    
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
     P1.update()
     P1.draw(DISPLAYSURF)
     DISPLAYSURF.blit(gui_heart[P1.health], (16, 16))
+    make_house()
     pygame.display.update()
     FramePerSec.tick(FPS)
